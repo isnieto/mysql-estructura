5b5d93b11fd90fd4d5d0d7f16c5d8dc41aa089bc
Ismael Nieto <ismael.nieto@gmail.com>
2021-03-11 09:18:30 +0100

    Exercises tienda complete

--- mysql-queries-tienda.sql
+++ mysql-queries-tienda.sql
@@ -1,86 +1,37 @@
--- Base de dades Tienda
-
--- 1. Llista el nom de tots els productos que hi ha en la taula producto.
     SELECT nombre FROM producto;
--- 2. Llista els noms i els preus de tots els productos de la taula producto.
     SELECT nombre, precio FROM producto;
--- 3. Llista totes les columnes de la taula producto.
     SELECT * FROM producto;
--- 4. Llista el nom dels productos, el preu en euros i el preu en dòlars estatunidencs (USD).
     SELECT nombre, CONCAT(precio, " €"), concat(round(precio*1.21,2), "$") FROM producto;
--- 5. Llista el nom dels productos, el preu en euros i el preu en dòlars estatunidencs (USD).
--- Utilitza els següents àlies per a les columnes: nom de producto, euros, dolars.
     SELECT nombre AS "nom del producte", CONCAT(precio, " €") AS "euros", concat(round(precio*1.21,2), "$") AS "dòlars" FROM producto;
--- 6. Llista els noms i els preus de tots els productos de la taula producto, convertint els noms a majúscula.
     SELECT  UPPER(nombre) AS "producto",  precio FROM producto;
--- 7. Llista els noms i els preus de tots els productos de la taula producto, convertint els noms a minúscula.
     SELECT  LOWER(nombre) AS "producto",  precio FROM producto;
--- 8. Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules 
--- els dos primers caràcters del nom del fabricant.
     SELECT nombre, SUBSTR(nombre,1, 2)  FROM fabricante;
--- 9. Llista els noms i els preus de tots els productos de la taula producto, arrodonint el valor del preu.
     SELECT nombre, round(precio, 1) AS "precio" FROM producto;
--- 10. Llista els noms i els preus de tots els productos de la taula producto, truncant el valor del preu
---  per a mostrar-lo sense cap xifra decimal.
     SELECT nombre, round(precio) AS "precio" FROM producto;
--- 11. Llista el codi dels fabricants que tenen productos en la taula producto.
     SELECT f.codigo FROM fabricante AS f, producto AS p  WHERE f.codigo = p.codigo_fabricante;
--- 12. Llista el codi dels fabricants que tenen productos en la taula producto, eliminant els codis que apareixen repetits.
     SELECT f.codigo FROM fabricante as f, producto as p  WHERE f.codigo = p.codigo_fabricante GROUP BY f.codigo;
--- 13. Llista els noms dels fabricants ordenats de manera ascendent.
     SELECT nombre FROM fabricante ORDER BY nombre ASC;
--- 14. Llista els noms dels fabricants ordenats de manera descendent.
     SELECT nombre FROM fabricante ORDER BY nombre DESC;
--- 15. Llista els noms dels productos ordenats en primer lloc pel nom de manera ascendent i en segon lloc pel preu de manera descendent.
     SELECT nombre, precio FROM fabricante ORDER BY nombre ASC,  precio DESC ;
--- 16. Retorna una llista amb les 5 primeres files de la taula fabricante.
     SELECT * FROM fabricante LIMIT 5;
--- 17. Retorna una llista amb 2 files a partir de la quarta fila de la taula fabricante. 
--- La quarta fila també s'ha d'incloure en la resposta.
     SELECT * FROM fabricante LIMIT 3, 2;
--- 18. Llista el nom i el preu del producto més barat. 
--- (Utilitzi solament les clàusules ORDER BY i LIMIT). NOTA: Aquí no podria usar MIN(preu), necessitaria GROUP BY
-   SELECT nombre, MIN(precio) AS 'precio más barato' FROM producto; 
--- 19. Llista el nom i el preu del producto més car. (Utilitzi solament les clàusules ORDER BY i LIMIT). 
--- NOTA: Aquí no podria usar MAX(preu), necessitaria GROUP BY.
+    SELECT nombre, MIN(precio) AS 'precio más barato' FROM producto; 
     SELECT nombre, precio AS 'precio más caro' FROM producto ORDER BY precio DESC LIMIT 1;
--- 21. Llista el nom de tots els productos del fabricant el codi de fabricant del qual és igual a 2.
     SELECT nombre FROM producto  WHERE codigo_fabricante = 2;
--- 22. Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades.
     SELECT p.nombre, p.precio, f.nombre FROM producto AS p, fabricante as f WHERE p.codigo_fabricante = f.codigo;
--- 23. Retorna una llista amb el nom del producte, preu i nom de fabricant de tots els productes de la base de dades. 
--- Ordeni el resultat pel nom del fabricador, per ordre alfabètic.
     SELECT p.nombre, p.precio, f.nombre FROM producto AS p, fabricante as f WHERE p.codigo_fabricante = f.codigo ORDER BY f.nombre ASC;
--- 24. Retorna una llista amb el codi del producte, nom del producte, codi del fabricador i nom del fabricador, 
--- de tots els productes de la base de dades.
     SELECT p.codigo_fabricante, p.nombre, f.codigo, f.nombre FROM producto AS p, fabricante as f WHERE p.codigo_fabricante = f.codigo;
--- 25. Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més barat.
     SELECT p.nombre, p.precio AS 'precio más barato', f.nombre AS 'fabricante' FROM producto AS p, fabricante as f WHERE p.codigo_fabricante = f.codigo ORDER BY precio ASC LIMIT 1;
--- 26. Retorna el nom del producte, el seu preu i el nom del seu fabricant, del producte més car.
     SELECT p.nombre, p.precio AS 'precio más caro', f.nombre AS 'fabricante' FROM producto AS p, fabricante as f WHERE p.codigo_fabricante = f.codigo ORDER BY precio DESC LIMIT 1;
--- 27. Retorna una llista de tots els productes del fabricador Lenovo.
     SELECT p.nombre, p.precio  FROM producto AS p JOIN fabricante AS f ON p.codigo_fabricante = f.codigo WHERE f.nombre = "Lenovo";
--- 28. Retorna una llista de tots els productes del fabricant Crucial que tinguin un preu major que 200€.
     SELECT p.nombre, max(p.precio) AS maximo  FROM producto AS p JOIN fabricante AS f ON p.codigo_fabricante = f.codigo WHERE f.nombre = "Crucial";
--- 29. Retorna un llistat amb tots els productes dels fabricants Asus, Hewlett-Packardy Seagate. Sense utilitzar l'operador IN.
     SELECT p.nombre, p.precio, f.nombre  FROM producto AS p JOIN fabricante AS f ON p.codigo_fabricante = f.codigo WHERE f.nombre = "Asus" || f.nombre = "Hewlett-Packard";
--- 30. Retorna un llistat amb tots els productes dels fabricants Asus, Hewlett-Packardy Seagate. Utilitzant l'operador IN.
     SELECT 	p.nombre, f.nombre as "fabricante" FROM fabricante f JOIN producto p ON f.codigo = p.codigo wHERE f.nombre IN('Asus', 'Seagate', 'Hewlett-Packard');
--- 31. Retorna un llistat amb el nom i el preu de tots els productes dels fabricants el nom dels quals acabi per la vocal e.
     SELECT 	p.nombre, p.precio, f.nombre as "fabricante" FROM fabricante f JOIN producto p ON f.codigo = p.codigo WHERE f.nombre LIKE '%e';
--- 32. Retorna un llistat amb el nom i el preu de tots els productes el nom de fabricant dels quals contingui el caràcter w en el seu nom.
     SELECT 	p.nombre, p.precio, f.nombre as "fabricante" FROM fabricante f JOIN producto p ON f.codigo = p.codigo WHERE f.nombre LIKE '%w%';
--- 33. Retorna un llistat amb el nom de producte, preu i nom de fabricant, de tots els productes que tinguin un preu major o igual a 180€. 
--- Ordeni el resultat en primer lloc pel preu (en ordre descendent) i en segon lloc pel nom (en ordre ascendent)
     SELECT 	p.nombre, p.precio, f.nombre as "fabricante" FROM fabricante f JOIN producto p ON f.codigo = p.codigo WHERE p.precio > 180 ORDER BY p.precio DESC, p.nombre ASC;
--- 34. Retorna un llistat amb el codi i el nom de fabricant, solament d'aquells fabricants que tenen productes associats en la base de dades.
     SELECT f.codigo, f.nombre FROM fabricante f JOIN producto  p ON f.codigo = p.codigo_fabricante GROUP BY f.nombre; ; 
--- 35. Retorna un llistat de tots els fabricants que existeixen en la base de dades, juntament amb els productes que té cadascun d'ells.
---  El llistat haurà de mostrar també aquells fabricants que no tenen productes associats.
     SELECT f.nombre, p.nombre FROM fabricante f LEFT JOIN producto  p ON f.codigo = p.codigo_fabricante ;
--- 36. Retorna un llistat on només apareguin aquells fabricants que no tenen cap producte associat.
     SELECT f.nombre, p.nombre FROM fabricante f LEFT JOIN producto  p ON f.codigo = p.codigo_fabricante WHERE p.codigo is NULL;
--- 37. Retorna tots els productes del fabricador Lenovo. (Sense utilitzar INNER JOIN).
     SELECT p.nombre, f.nombre from fabricante f,  producto p WHERE f.nombre = 'Lenovo' and f.codigo =  p.codigo_fabricante ;
--- 38. Retorna totes les dades dels productes que tenen el mateix preu que el producte més car del fabricador Lenovo. (Sense utilitzar INNER JOIN).
     SELECT p.nombre, f.nombre, p.precio from fabricante f,  producto p WHERE  p.precio = (SELECT max(p.precio) from fabricante f,  producto p WHERE f.nombre = 'Lenovo' ) AND f.codigo =  p.codigo_fabricante;
--- mysql-queries-universidad.sql
+++ mysql-queries-universidad.sql
@@ -1,106 +1,20 @@
- 
--- Base de dades Universidad
-
--- 1. Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els alumnes. El llistat haurà d'estar ordenat alfabèticament 
--- de menor a major pel primer cognom, segon cognom i nom.
-    SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1 ASC, apellido2 ASC, nombre ASC;
-
--- 2. Escriuel nom i els dos cognoms dels alumnes que no han donat d'alta el seu número de telèfon en la base de dades.
-    SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'alumno' && telefono is NULL ORDER BY nombre ASC;
-
--- 3. Retorna el llistat dels alumnes que van néixer en 1999.
-   SELECT nombre, apellido1, apellido2 FROM persona WHERE YEAR(fecha_nacimiento) = '1999';
-
--- 4. Retorna el llistat de professors que no han donat d'alta el seu número de telèfon en la base de dades i a més la seva nif acaba en K.
-    SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'profesor' && telefono is NULL && nif LIKE '%K';
-
--- 5. Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7.
-    SELECT nombre FROM asignatura WHERE curso = 3 && cuatrimestre = 1;
-
--- 6. Retorna un llistat dels professors juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, 
--- primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom.
-    SELECT p.apellido1, p.apellido2, p.nombre, d.nombre FROM persona AS p, departamento AS d, profesor AS f  WHERE d.id = f.id_departamento && p.id = f.id_profesor ORDER BY  p.apellido1 ASC, p.apellido2 ASC, p.nombre DESC; 
-
--- 7. Retorna un llistat amb el nom de les assignatures, any d'inici i any de fi del curs escolar de l'alumne amb nif 26902806M.
-     SELECT  c.anyo_inicio, c.anyo_fin, s.nombre FROM persona p JOIN alumno_se_matricula_asignatura a ON p.id = a.id_alumno JOIN asignatura s on a.id_asignatura = s.id JOIN curso_escolar c ON c.id = s.curso WHERE nif LIKE  '26902806M';
-
--- 8. Retorna un llistat amb el nom de tots els departaments que tenen professors que imparteixen alguna assignatura en el Grau en Enginyeria Informàtica (Pla 2015).
-    ????select * from profesor p left join departamento d on p.id_departamento = d.id left join asignatura a on p.id_profesor = a.id_profesor;
-
--- 9. Retorna un llistat amb tots els alumnes que s'han matriculat en alguna assignatura durant el curs escolar 2018/2019.
-    SELECT p.nombre, p.apellido1, p.apellido2, c.anyo_inicio, c.anyo_fin FROM alumno_se_matricula_asignatura a JOIN persona p ON p.id = a.id_alumno JOIN curso_escolar c ON c.id = a.id_curso_escolar WHERE a.id_curso_escolar = 5 GROUP BY a.id_alumno;
-
-
------------------------------------------------
--------- Resolgui les 6 següents consultes utilitzant les clàusules LEFT JOIN i RIGHT JOIN.
-
--- 10. Retorna un llistat amb els noms de tots els professors i els departaments que tenen vinculats. 
--- El llistat també ha de mostrar aquells professors que no tenen cap departament associat. El llistat ha de retornar quatre columnes,
--- nom del departament, primer cognom, segon cognom i nom del professor. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, 
--- cognoms i el nom.
-   SELECT d.nombre, p.apellido1, p.apellido2, p.nombre FROM persona p LEFT JOIN profesor f ON p.id = f.id_profesor LEFT JOIN departamento d ON f.id_departamento = d.id ORDER BY d.nombre, p.apellido1, p.apellido2, p.nombre ASC;
-
--- 11. Retorna un llistat amb els professors que no estan associats a un departament.
-    SELECT  p.nombre, p.apellido1, f.id_departamento FROM persona p LEFT JOIN profesor f ON p.id = f.id_profesor WHERE f.id_departamento is NULL;
-
--- 12. Retorna un llistat amb els departaments que no tenen professors associats.
-    SELECT d.nombre FROM departamento d LEFT JOIN profesor p on d.id = p.id_departamento WHERE p.id_departamento is NULL;
-
--- 13. Retorna un llistat amb els professors que no imparteixen cap assignatura.
-    SELECT p.apellido1, p.apellido2, p.nombre FROM persona p LEFT JOIN asignatura a ON p.id = a.id_profesor WHERE a.id_profesor IS NULL ORDER BY p.apellido1, p.apellido2, p.nombre ASC;
-
--- 14. Retorna un llistat amb les assignatures que no tenen un professor assignat.
-    SELECT a.id, a.nombre, a.id_profesor FROM asignatura a LEFT JOIN persona p ON a.id_profesor = p.id WHERE a.id_profesor is NULL;
-
--- 15- Retorna un llistat amb tots els departaments que no han impartit assignatures en cap curs escolar.
- ????  select d.nombre from asignatura a left join profesor p on a.id_profesor = p.id_profesor left join departamento d on p.id_departamento = d.id;
-
-----------------------------------------------------------
--- Consultes resum:
-
--- 16. Retorna el nombre total d'alumnes que hi ha.
-    SELECT count(id) AS numero_total_alumnos FROM persona WHERE tipo = 'alumno' limit 10;
-
--- 17. Calcula quants alumnes van néixer en 1999.
-    SELECT count(id) AS 'nacidos_en_1999' FROM persona WHERE tipo = 'alumno' && fecha_nacimiento like '%1999%';
-
--- 18. Calcula quants professors hi ha en cada departament. El resultat només ha de mostrar dues columnes, una amb el nom del departament i una altra amb el nombre
--- de professors que hi ha en aquest departament. El resultat només ha d'incloure els departaments que tenen professors associats i haurà d'estar ordenat de major a 
--- menor pel nombre de professors.
-   SELECT d.nombre, count(p.id_profesor) AS 'numero_profesores' FROM profesor p JOIN departamento d ON p.id_departamento = d.id GROUP BY p.id_departamento ORDER BY  count(p.id_profesor) DESC;
-
-
--- 19. Retorna un llistat amb tots els departaments i el nombre de professors que hi ha en cadascun d'ells.
---  Tingui en compte que poden existir departaments que no tenen professors associats. Aquests departaments també han d'aparèixer en el llistat.
-????
-SELECT
-	d.nombre,
-	p.id_profesor,
-	p.id_departamento 
-FROM
-	departamento d
-LEFT JOIN profesor p ON
-	d.id = p.id_departamento
-GROUP by d.id;
-
-
--- 20. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. 
--- Tingui en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d'aparèixer en el llistat. 
--- El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures.
-    select nombre, numero from ( select g.nombre as nombre, count(a.nombre) AS numero  from grado g, asignatura a where g.id = a.id_grado group by g.nombre
-    ) where numero>40;
-
--- 21. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades.
-    SELECT 	g.nombre, a.nombre, count(g.nombre) as nr FROM grado g join asignatura a on g.id = a.id_grado GROUP BY g.nombre HAVING 	count(g.nombre) > 40;
-
--- 22. Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus.
-    SELECT 	g.nombre, a.tipo, count(a.creditos) As 'nr.creditos' FROM grado g JOIN asignatura a ON g.id = a.id_grado GROUP BY a.creditos;
-
--- 23. Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats.
-
--- 24. Retorna un llistat amb el nombre d'assignatures que imparteix cada professor. El llistat ha de tenir en compte aquells professors que no imparteixen cap assignatura. El resultat mostrarà cinc columnes: id, nom, primer cognom, segon cognom i nombre d'assignatures. El resultat estarà ordenat de major a menor pel nombre d'assignatures.
-
--- 25. Retorna totes les dades de l'alumne més jove.
-
--- 26. Retorna un llistat amb els professors que tenen un departament associat i que no imparteixen cap assignatura.
+SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1 ASC, apellido2 ASC, nombre ASC;
+SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'alumno' && telefono is NULL ORDER BY nombre ASC;
+SELECT nombre, apellido1, apellido2 FROM persona WHERE YEAR(fecha_nacimiento) = '1999';
+SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'profesor' && telefono is NULL && nif LIKE '%K';
+SELECT nombre FROM asignatura WHERE curso = 3 && cuatrimestre = 1;
+SELECT p.apellido1, p.apellido2, p.nombre, d.nombre FROM persona AS p, departamento AS d, profesor AS f  WHERE d.id = f.id_departamento && p.id = f.id_profesor ORDER BY  p.apellido1 ASC, p.apellido2 ASC, p.nombre DESC; 
+SELECT c.anyo_inicio, c.anyo_fin, s.nombre FROM persona p JOIN alumno_se_matricula_asignatura a ON p.id = a.id_alumno JOIN asignatura s on a.id_asignatura = s.id JOIN curso_escolar c ON c.id = s.curso WHERE nif LIKE  '26902806M';
+SELECT p.nombre, p.apellido1, p.apellido2, c.anyo_inicio, c.anyo_fin FROM alumno_se_matricula_asignatura a JOIN persona p ON p.id = a.id_alumno JOIN curso_escolar c ON c.id = a.id_curso_escolar WHERE a.id_curso_escolar = 5 GROUP BY a.id_alumno;
+SELECT d.nombre, p.apellido1, p.apellido2, p.nombre FROM persona p LEFT JOIN profesor f ON p.id = f.id_profesor LEFT JOIN departamento d ON f.id_departamento = d.id ORDER BY d.nombre, p.apellido1, p.apellido2, p.nombre ASC;
+SELECT p.nombre, p.apellido1, f.id_departamento FROM persona p LEFT JOIN profesor f ON p.id = f.id_profesor WHERE f.id_departamento is NULL;
+SELECT d.nombre FROM departamento d LEFT JOIN profesor p on d.id = p.id_departamento WHERE p.id_departamento is NULL;
+SELECT p.apellido1, p.apellido2, p.nombre FROM persona p LEFT JOIN asignatura a ON p.id = a.id_profesor WHERE a.id_profesor IS NULL ORDER BY p.apellido1, p.apellido2, p.nombre ASC;
+SELECT a.id, a.nombre, a.id_profesor FROM asignatura a LEFT JOIN persona p ON a.id_profesor = p.id WHERE a.id_profesor is NULL;
+SELECT count(id) AS numero_total_alumnos FROM persona WHERE tipo = 'alumno' limit 10;
+SELECT count(id) AS 'nacidos_en_1999' FROM persona WHERE tipo = 'alumno' && fecha_nacimiento like '%1999%';
+SELECT d.nombre, count(p.id_profesor) AS 'numero_profesores' FROM profesor p JOIN departamento d ON p.id_departamento = d.id GROUP BY p.id_departamento ORDER BY  count(p.id_profesor) DESC;
+SELECT nombre, numero FROM (SELECT g.nombre as nombre, count(a.nombre) AS numero  FROM grado g, asignatura a WHERE g.id = a.id_grado group by g.nombre) WHERE numero>40;
+SELECT g.nombre, a.nombre, count(g.nombre) as nr FROM grado g join asignatura a on g.id = a.id_grado GROUP BY g.nombre HAVING 	count(g.nombre) > 40;
+SELECT g.nombre, a.tipo, count(a.creditos) As 'nr.creditos' FROM grado g JOIN asignatura a ON g.id = a.id_grado GROUP BY a.creditos;
 
